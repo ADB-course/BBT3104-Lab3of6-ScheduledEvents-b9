bf48e0653cbf92bd9ebbc09a3176034fefde6b39
TimWachira <166481796+TimWachira@users.noreply.github.com>
2024-10-01 12:51:14 +0300

    Update lab_submission.sql
    Signed-off-by: TimWachira <166481796+TimWachira@users.noreply.github.com>

--- lab_submission.sql
+++ lab_submission.sql
@@ -4,3 +4,10 @@ CREATE TABLE `customer_service_kpi(
   PRIMARY KEY(`customer_service_KPI_timestamp`));
 
 -- CREATE EVN_average_customer_waiting_time_every_1_hour--
+CREATE EVENT `EVN_average_customer_waiting_time_every_1_hour`
+ON SCHEDULE EVERY 1 HOUR
+DO
+INSERT INTO `customer_service_kpi` (customer_service_KPI_average_waiting_time_minutes)
+SELECT AVG(TIMESTAMPDIFF(MINUTE, ticket_created_timestamp, ticket_resolved_timestamp)) 
+FROM `customer_service_ticket`
+WHERE ticket_created_timestamp >= NOW() - INTERVAL 1 HOUR;
